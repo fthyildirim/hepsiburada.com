bdbc9a2fbf7ced4ee65d1214e12de0552e59e183
fatihyldrm976 <66695748+fatihyldrm976@users.noreply.github.com>
2021-05-07 19:59:24 +0300

    gittigidiyor.com
    deneme amaçlı yapılmıştır(ÖRNEKTİR)

---
+++ js/scirpt.js
@@ -0,0 +1,27 @@
+function mOver1 (obj){
+    obj.src="img/java2.png"
+}
+function mOut1(obj){
+    obj.src="img/java1.png"
+}
+
+function mOver2 (obj){
+    obj.src="img/java4.png"
+}
+function mOut2(obj){
+    obj.src="img/java3.png"
+}
+
+function mOver3 (obj){
+    obj.src="img/java6.png"
+}
+function mOut3(obj){
+    obj.src="img/java5.png"
+}
+
+function mOver4 (obj){
+    obj.src="img/java8.png"
+}
+function mOut4(obj){
+    obj.src="img/java7.png"
+}
